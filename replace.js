0f2d27e01927ff9decf16a1800d0cc8268476585
Daniel Aristizabal Garcia <quesomaik@gmail.com>
2024-02-04 22:16:12 -0500

    se ha creado un nuevo alert

--- replace.js
+++ replace.js
@@ -5,4 +5,8 @@ para.appendChild(darte);
 
 const element = document.getElementById("deb");
 const child = document.getElementById("p1");
+/* con este metodo podemos cambiar un elemento de otro elemnto pasando como parametro
+el elemnto se sera sustituirá y de segundo el que se eliminara de elemento padre*/
 element.replaceChild(para, child);
+
+alert("hola replace");
